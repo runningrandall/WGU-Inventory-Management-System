021bac34cc0f8b841bb657641580f1d7898a6c56
Randall Adams <randall.adams@outlook.com>
2020-12-31 14:29:40 -0700

    add javadocs

---
+++ javadocs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"Main","l":"InHouse"},{"p":"Main","l":"Inventory"},{"p":"Main","l":"Main"},{"p":"Main","l":"Outsourced"},{"p":"Main","l":"Part"},{"p":"Elements","l":"PartsTable"},{"p":"Main","l":"Product"},{"p":"Elements","l":"ProductsTable"}]
